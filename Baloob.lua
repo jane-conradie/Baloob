74a314c7c36b401a41b4e580ae679d1db6f574cb
jane_conradie <jconradie1998@gmail.com>
2024-01-16 17:58:18 +0200

    Initial commit

---
+++ Baloob.lua
@@ -0,0 +1,50 @@
+Baloob = Class {}
+
+local bopSpeed = 5
+local bopDirection = 'down'
+local bopStoppingPoint = 2
+
+local baloobYPosition = 0
+
+-- table for Baloob containing its properties
+function Baloob:init()
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- load in image
+    self.image = love.graphics.newImage('assets/sprites/baloob_um.png')
+
+    -- get image dimensions
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    -- where you want this sprite to spawn, in this case center in screen
+    -- take into account image width and height as well
+    self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+    self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+    -- store initial baloob's y position
+    baloobYPosition = self.y
+end
+
+-- this will trigger every frame
+function Baloob:render(dt)
+
+    -- adding little bop animation to make baloob feel more alive
+    if bopDirection == 'down' then
+        if self.y < (baloobYPosition + bopStoppingPoint) then
+            self.y = self.y + bopSpeed * dt
+        else
+            bopDirection = 'up'
+        end
+    else
+        if self.y > (baloobYPosition - bopStoppingPoint) then
+            self.y = self.y - bopSpeed * dt
+        else
+            bopDirection = 'down'
+        end
+    end
+
+    -- make baloon bop up and down to give some animation and realism
+    love.graphics.draw(self.image, self.x, self.y)
+end
---
+++ main.lua
@@ -0,0 +1,80 @@
+push = require 'push'
+
+Class = require 'class'
+
+-- import our player Baloob
+require 'Baloob'
+
+-- dimensions we want
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- adding our virtual dimensions
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+-- load the background image
+local background = love.graphics.newImage('assets/scenery/background.png')
+local backgroundScroll = 0
+
+-- load the ground
+local ground = love.graphics.newImage('assets/scenery/ground.png')
+local groundScroll = 0
+
+-- speeds at which to scroll each scenery item
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+-- point at which to return each item back to 0 once it reaches this
+local BACKGROUND_LOOP_POINT = 413
+
+local baloob = Baloob()
+
+local deltaTime = 0
+
+function love.load()
+    -- setting filter to nearest, nearest to give a retro look
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    love.window.setTitle("Baloob")
+
+    -- settings for screen
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = true,
+        vsync = true
+    })
+
+    -- load some game sounds into this
+end
+
+function love.update(dt)
+    -- update the scenery items to scroll per frame for parallax scrolling
+    -- will count up little by little (+0.xx), will hit each whole number twice (1, 1, 2, 2) until it reaches 413 and reset to 0
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOP_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOP_POINT
+
+    deltaTime = dt
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.draw()
+    push:start()
+
+    -- draw background at top left of screen
+    love.graphics.draw(background, -backgroundScroll, 0)
+
+    -- draw ground a bit higher than end of screen
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 14)
+
+    print(deltaTime)
+
+    -- render player
+    baloob:render(deltaTime)
+
+    push:finish()
+end
